3445e2d1b42c286f6b418d47cb5b129a5614ded8
Padexing <83568630+Padexing@users.noreply.github.com>
2021-09-11 20:03:40 +0800

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -14,8 +14,8 @@ extension = {
             "model": "MI 12",
             "deviceId": str(uuid.uuid4()),
             "appVersion": "9.0.0",
-            "lon": 22.688269,
-            "lat": 114.2117,
+            "lon": 114.222966,
+            "lat": 22.691566,
             "userId": str(random.randint(1,50))+"1a133"+str(random.randint(1,50)),
         }
 print(DESEncrypt(json.dumps(extension)))
