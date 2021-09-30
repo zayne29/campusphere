847c7384e3e58ac65befcc8478265d3bfe17711f
Padexing <83568630+Padexing@users.noreply.github.com>
2021-09-30 19:56:22 +0800

    Update cpdaily.py

--- cpdaily.py
+++ cpdaily.py
@@ -12,15 +12,8 @@ def getLogin(username,password):
     global session
     session=requests.session()
     g_response=session.get(url)
-    # print(g_response.text)
     soup = bs(g_response.text, 'html.parser')
     dt=soup.find_all('input',type="hidden")
-
-    # scripttxt = soup.find_all("script")
-    # # print(scripttxt)
-    # pattern = re.compile('var pwdDefaultEncryptSalt = "(.*)";')
-    # pwdDefaultEncryptSalt = pattern.findall(str(scripttxt))
-    # print(dt)
     d=[]
     for x in dt:
         d.append(x.get('value'))
@@ -40,25 +33,15 @@ def getLogin(username,password):
     }
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded',
-               'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.1; SM-N9300 Build/R2MZBS) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.6985.79 Mobile Safari/537.36 okhttp/3.12.4'
+               'User-Agent': ua
                }
     session.post(url,data=data,headers=headers)
 
 
-
-
-
-
-
-
 def getInfos():
     url = "https://sziit.campusphere.net/wec-counselor-sign-apps/stu/sign/getStuSignInfosInOneDay"
-
-
-    response = session.post(url, headers={'Content-Type': 'application/json;charset=UTF-8'}, data="{}")
+    response = session.post(url, headers={'Content-Type': 'application/json;charset=UTF-8','User-Agent': ua}, data="{}")
     d = json.loads(response.text)
-
-
     global signInstanceWid
     global signWid
     signInstanceWid=d["datas"]["unSignedTasks"][0]["signInstanceWid"]
@@ -71,29 +54,24 @@ def getForm(signInstanceWid,signWid):
     payload = "\"signInstanceWid\": \"{a}\",\"signWid\": \"{b}\"".format(a=signInstanceWid,b=signWid)
     payload="{"+payload+"}"
 
-    response = session.post(url, headers={'Content-Type': 'application/json;charset=UTF-8'}, data=payload)
-    # print(response.text)
+    response = session.post(url, headers={'Content-Type': 'application/json;charset=UTF-8','User-Agent': ua}, data=payload)
     d = json.loads(response.text)
-
-
     global awid
     global bwid
     awid=d["datas"]["extraField"][0]["extraFieldItems"][1]["wid"]
     bwid=d["datas"]["extraField"][1]["extraFieldItems"][1]["wid"]
 
-    # print(awid)
-    # print(bwid)
 
 
 def submitForm(signInstanceWid,awid,bwid,extension):
     url = "https://sziit.campusphere.net/wec-counselor-sign-apps/stu/sign/submitSign"
     header= {
     'Content-Type': 'application/json;charset=UTF-8',
+    'User-Agent': ua,
     'Cpdaily-Extension':extension
     }
-    payload={"longitude":114.222966,"latitude":22.691566,"isMalposition":0,"abnormalReason":"","signPhotoUrl":"","isNeedExtra":1,"position":"中国广东省深圳市龙岗区龙格路301号","uaIsCpadaily":"true","signInstanceWid":str(signInstanceWid),"signVersion":"1.0.0","extraFieldItems":[{"extraFieldItemValue":"否","extraFieldItemWid":str(awid)},{"extraFieldItemValue":"否","extraFieldItemWid":str(bwid)}]}
+    payload={"longitude":114.222966,"latitude":22.691566,"isMalposition":0,"abnormalReason":"","signPhotoUrl":"","isNeedExtra":1,"position":"中国广东省深圳市龙岗区龙格路301号","uaIsCpadaily":"true","signInstanceWid":str(signInstanceWid),"osgnVersion":"1.0.0","extraFieldItems":[{"extraFieldItemValue":"否","extraFieldItemWid":str(awid)},{"extraFieldItemValue":"否","extraFieldItemWid":str(bwid)}]}
     response = session.post(url, headers=header, json=payload)
-    # print(payload)
     print(response.text)
 
 
@@ -105,5 +83,3 @@ if __name__ == '__main__':
     getInfos()
     getForm(signInstanceWid,signWid)
     submitForm(signInstanceWid,awid,bwid,extension)
-
-# print(m_signInstanceWid, m_signWid, awid, bwid)
